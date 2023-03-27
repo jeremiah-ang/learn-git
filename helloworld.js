a16a3dcce3b6fb46be477195bc1fb5bec8418ae5
Jeremiah Ang (SP) <jeremiah_ang@sp.edu.sg>
2023-03-27 17:09:08 +0800

    Display credits at the end

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,11 @@ console.log('Start');
 let i = 0;
 while (i < 10) {
     console.log(`Hello World ${i + 1}`);
-    console.log(`========`);
+    console.log(`---------`);
     i++;
 }
 console.log('The end!');
+console.log('Credits:');
+for (let i = 0; i < 10; i++) {
+    console.log(`Name ${i}`);
+}
